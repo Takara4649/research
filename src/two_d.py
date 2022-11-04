72377e7896fef8286e9f7cd305b87047684afa94
Takara4649 <ngthkuei@i.softbank.jp>
2022-11-04 23:07:34 +0900

    Add:two_d.py

---
+++ src/two_d.py
@@ -0,0 +1,36 @@
+import random
+import itertools
+def lx(n:int, p:float, q:float,values_x: list)-> list[float]:
+    """２次元ランダムウォークのx座標
+
+    Args:
+        n (int): ステップ数
+        p (float): 確率
+        q (float): 確率
+        values_x (list): xの取る値
+
+    Returns:
+        list[float]: x座標
+    """
+    w_x = [p, q]
+    lsx = random.choices(values_x, weights=w_x, k=n)
+    #lsx = lsx[3:]
+    result_x=[result -i*p  for result, i in zip(itertools.accumulate(lsx), range(0,n))]
+    return result_x
+def ly(n:int, r:float, s:float,values_y: list)-> list[float]:
+    """２次元ランダムウォークのy座標
+
+    Args:
+        n (int): ステップ数
+        r (float): 確率
+        s (float): 確率
+        values_y (list): yの取る値
+
+    Returns:
+        list[float]: y座標
+    """
+    w_y = [r, s]
+    lsy = random.choices(values_y, weights=w_y, k=n)
+    #lsy = lsy[3:]
+    result_y=[result -i*r  for result, i in zip(itertools.accumulate(lsy), range(0,n))]
+    return result_y
